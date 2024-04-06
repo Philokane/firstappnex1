1dda4208499bfaebacb71b0bc80eb73424e74871
Phil Carroll-O'Kane <pcarrollokane@fastly.com>
2024-04-06 17:03:29 +0100

    update responses

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,14 +1,4 @@
+export default function Page() {
 
-
-// export default function Page() {
-    
-//     return <p>Takehome Test April 2024</p>
-// }
-
-
-export default function Hello() {
-    return (
-        <h1>Hello World</h1>
-    )
-    <p>asd asd </p>
+    return <p>Takehome Test April 2024</p>
 }
